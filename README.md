ee30cccefeb64126dc26cb5d578eb19c3b465411
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-02-20 12:38:02 +0000

    Retrieves Auth'd user data from GH API

---
+++ README.md
@@ -0,0 +1,11 @@
+# Github API App
+A simple python program which uses the Github API to do things.
+The projects is made to gain more experience with the Github API and to start looking 
+into the use of Poetry to better manage projects and dependencies.
+
+## Setup
+1. Install Poetry
+2. Create a Virtual Environement and Activate it
+3. Run `Poetry install` to install all the required dependencies
+
+You can now run the project.
--- githubapiapp/__init__.py
+++ githubapiapp/__init__.py
@@ -1,1 +1,42 @@
-print("Hello World")
+import requests
+import dotenv
+import os
+from PIL import Image
+
+class APIHandler():
+    def __init__(self) -> None:
+        dotenv.load_dotenv()
+        token = os.getenv("TOKEN")
+        self.headers = {"Authorization": "token " + token}
+
+    def get(self, url):
+        url = "https://api.github.com" + url
+        return requests.get(url=url, headers=self.headers)
+
+if __name__ == "__main__":
+    if not os.path.exists(".env"):
+        print("Github Access Token Required.")
+        token = input("Please enter a Github Access Token for your Account: ")
+
+        if token != "":
+            with open(".env", "w") as f:
+                f.write(f'TOKEN="{token}"')
+
+    gh = APIHandler()
+    userData = gh.get("/user")
+
+    if userData.status_code == 200:
+        json = userData.json()
+
+        # Shows User Profile Image
+        # userPhoto = requests.get(json["avatar_url"])
+        # with open("image.jpg", "wb") as f:
+        #     f.write(userPhoto.content)
+
+        # img = Image.open("image.jpg")
+        # img.show()
+
+
+        print(f"Authenticated as: {json["name"]} ({json["login"]})")
+    else:
+        print(f"Error Getting User Data. Error {userData.status_code}, {userData.json()["message"]}")
