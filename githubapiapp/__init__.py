3776778cc5a4c1112e1ea68bc8fe1c6b57c9480b
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-02-21 17:20:29 +0000

    Can create and delete repos.

--- githubapiapp/__init__.py
+++ githubapiapp/__init__.py
@@ -2,6 +2,7 @@ import requests
 import dotenv
 import os
 import datetime
+# import pprint
 # from PIL import Image
 
 
@@ -15,19 +16,28 @@ class APIHandler():
         token = os.getenv("TOKEN")
         self.headers = {"Authorization": "token " + token}
 
-    def get(self, url, addPrefix: bool = True):
+    def get(self, url, params, addPrefix: bool = True):
         if addPrefix:
             url = "https://api.github.com" + url
-        return requests.get(url=url, headers=self.headers)
+        return requests.get(url=url, headers=self.headers, params=params)
     
-    def patch(self, url, params):
+    def patch(self, url, params, addPrefix: bool = True):
         url = "https://api.github.com" + url
         return requests.patch(url=url, headers=self.headers, json=params)
+    
+    def post(self, url, params, addPrefix: bool = True):
+        url = "https://api.github.com" + url
+        return requests.post(url=url, headers=self.headers, json=params)
+    
+    def delete(self, url, addPrefix: bool = True):
+        url = "https://api.github.com" + url
+
+        return requests.delete(url=url, headers=self.headers)
 
 # User Functions
 def viewProfile(json: dict):
-    print(json)
-    print("\n")
+    # print(json)
+    # print("\n")
 
     # Basic User Data
 
@@ -73,7 +83,62 @@ def viewFollows(json: dict):
         for following in followingJson:
             print(f"- {following["login"]}")
     else:
-        print(f"Error Getting Follower Data. Error {response.status_code}, {response.json()["message"]}")
+        print(f"Error Getting Following Data. Error {response.status_code}, {response.json()["message"]}")
+
+def viewRepos(json: dict):
+    params = {
+        "affiliation": "owner"
+    }
+
+    response = gh.get("/user/repos", params=params)
+    if response.status_code == 200:
+        reposJson = response.json()
+
+        # pprint.pprint(reposJson[0])
+
+        for count, repo in enumerate(reposJson):
+            print(f"{count+1}. {repo["name"]}")
+            print(f"Description: {repo["description"]}")
+            print(f"Visibility: {repo["visibility"]}")
+            print(f"Owner: {repo["owner"]["login"]}")
+            print(f"Link: {repo["html_url"]} \n")
+
+        
+    else:
+        print(f"Error Getting Repos. Error {response.status_code}, {response.json()["message"]}")
+
+def createRepo(username: str):
+    url = f"/user/repos"
+    
+    repoName = str(input("Please enter the Repo Name: "))
+    repoDesc = str(input("Please enter the Repo Description: "))
+    repoPriv = str(input("Is the Repo Private? (True/False) ")).lower()
+
+    repoPriv = True if repoPriv == "true" else False
+
+    params = {
+        "name": repoName,
+        "description": repoDesc,
+        "private": repoPriv
+    }
+
+    response = gh.post(url, params)
+
+    if response.status_code == 201:
+        print(f"Success. Repository Created at https://github.com/TotalDwarf03/{repoName}")
+    else:
+        print(f"Error Creating Repo. Error {response.status_code}, {response.json()["message"]}")
+
+def deleteRepo():
+    username = str(input("Enter the Repo Owner: "))
+    repo = str(input("Enter the Repo Name: "))
+
+    url = f"/repos/{username}/{repo}"
+
+    response = gh.delete(url)
+
+    print(response)
+
 
 def editBio():
     oldBio = gh.get("/user").json()["bio"]
@@ -97,7 +162,7 @@ if __name__ == "__main__":
                 f.write(f'TOKEN="{token}"')
 
     gh = APIHandler()
-    userData = gh.get("/user")
+    userData = gh.get("/user", {})
 
     if userData.status_code == 200:
         json = userData.json()
@@ -109,23 +174,32 @@ if __name__ == "__main__":
         exitCode = False
 
         while not exitCode:
-            selection = int(input(""" \n
+            selection = input(""" \n
 Please Select an Option:
 1. View Profile
 2. View Followers/Following
-5. Update Bio
+3. View Repos
+4. Create Repo
+5. Delete Repo
+6. Update Bio
                                   
 Type -1 to Quit.
-"""))
+""")
             clearTerminal()
             match selection:
-                case 1:
+                case "1":
                     viewProfile(json)
-                case 2:
+                case "2":
                     viewFollows(json)
-                case 5:
+                case "3":
+                    viewRepos(json)
+                case "4":
+                    createRepo(json["login"])
+                case "5":
+                    deleteRepo()
+                case "6":
                     editBio()
-                case -1:
+                case "-1":
                     exitCode = True
                 # Default
                 case _:
