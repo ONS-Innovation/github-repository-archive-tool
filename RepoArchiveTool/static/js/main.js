b69e48e52890148c7c0b36266f50d14a971e8e23
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-03-27 13:14:45 +0000

    fixed navbar resizing

--- RepoArchiveTool/static/js/main.js
+++ RepoArchiveTool/static/js/main.js
@@ -1,3 +1,29 @@
+// Event listener to change navbar when screen resizes
+window.addEventListener('resize', () => {
+    movePATForm();
+});
+
+// Moves the PAT token form between the navbar and inputBar
+function movePATForm(){
+    screenWidth = window.innerWidth;
+    tokenInput = document.getElementById("tokenInput");
+    themeSwitch = document.getElementById("themeSwitch");
+    inputBar = document.getElementById("inputBar");
+    navbar = document.getElementById("navbar");
+
+    // Get a navInput's parent's id to check if it's in the navbar or inputBar
+    parentID = tokenInput.parentElement.id;
+
+    if(screenWidth <= 990 && parentID == "navbar"){
+        inputBar.appendChild(tokenInput);
+    }
+    
+    if(screenWidth > 990 && parentID == "inputBar"){
+        navbar.insertBefore(tokenInput, themeSwitch);
+    }
+}
+
+// Loads theme from sessionStorage
 function loadTheme(){
     if(sessionStorage.theme == "dark"){
         document.documentElement.setAttribute("data-bs-theme", "dark");
@@ -9,6 +35,7 @@ function loadTheme(){
     }
 }
 
+// Switches the theme based on the navbar checkbox
 function toggleTheme(){
     // Get current theme
     theme = document.documentElement.getAttribute("data-bs-theme");
