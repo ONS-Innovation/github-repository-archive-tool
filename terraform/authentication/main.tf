2b429abbb2ecae5365c2ef577bf5e6a91679848d
Steve Gibbard <steve.gibbard@ons.gov.uk>
2024-06-25 19:16:19 +0100

    Split terraform into env configs

--- terraform/authentication/main.tf
+++ terraform/authentication/main.tf
@@ -1,10 +1,11 @@
 # Create a service running on fargate with a task definition and service definition
 terraform {
   backend "s3" {
-    bucket         = "sdp-dev-tf-state"
-    key            = "sdp-dev-ecs-github-audit-auth/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-state-lock"
+    # Backend is selected using terraform init -backend-config=path/to/backend-<env>.tfbackend
+    # bucket         = "sdp-dev-tf-state"
+    # key            = "sdp-dev-ecs-github-audit-auth/terraform.tfstate"
+    # region         = "eu-west-2"
+    # dynamodb_table = "terraform-state-lock"
   }
 
 }
--- terraform/service/data.tf
+++ terraform/service/data.tf
@@ -2,8 +2,8 @@
 data "terraform_remote_state" "ecs_infrastructure" {
   backend = "s3"
   config = {
-    bucket = "sdp-dev-tf-state"
-    key    = "sdp-dev-ecs-infra/terraform.tfstate"
+    bucket = "${var.domain}-tf-state"
+    key    = "${var.domain}-ecs-infra/terraform.tfstate"
     region = "eu-west-2"
   }
 }
@@ -11,8 +11,8 @@ data "terraform_remote_state" "ecs_infrastructure" {
 data "terraform_remote_state" "ecs_auth" {
   backend = "s3"
   config = {
-    bucket = "sdp-dev-tf-state"
-    key    = "sdp-dev-ecs-github-audit-auth/terraform.tfstate"
+    bucket = "${var.domain}-tf-state"
+    key    = "${var.domain}-ecs-github-audit-auth/terraform.tfstate"
     region = "eu-west-2"
   }
 }
